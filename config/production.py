7ee7b7090f45611d3fcca3f51ce3b8f205e87c4c
Sangmin Lee <sangminlee7648@gmail.com>
2021-02-21 17:10:37 +0900

    added logging feature

--- config/production.py
+++ config/production.py
@@ -1,4 +1,5 @@
 from config.default import *
+from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'flatagram.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -6,3 +7,26 @@ SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
 
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'flatagram/static/img/post/')
 ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'gif', 'png']
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
